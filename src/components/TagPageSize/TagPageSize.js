90674185888bc18e34b99833668f72395cf860b7
Maciej Necka <macieknecka@gmail.com>
2024-04-02 20:59:38 +0200

    Numeric Page size added

--- src/components/TagPageSize/TagPageSize.js
+++ src/components/TagPageSize/TagPageSize.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import { FormControl, TextField } from '@mui/material';
 
 const TagPageSize = ({ pageSize, onPageSizeChange }) => (
   <FormControl
     variant="outlined"
     size="small"
-    style={{ margin: '10px', minWidth: '120px' }}
+    style={{ margin: '10px', maxWidth: '100px' }}
   >
-    <InputLabel>Page Size</InputLabel>
-    <Select value={pageSize} onChange={onPageSizeChange} label="Page Size">
-      {[10, 20, 30, 40, 50].map((size) => (
-        <MenuItem key={size} value={size}>
-          {size}
-        </MenuItem>
-      ))}
-    </Select>
+    <TextField
+      label="Page Size"
+      type="number"
+      variant="outlined"
+      size="small"
+      value={pageSize}
+      onChange={onPageSizeChange}
+      inputProps={{ min: '10', max: '100', step: '10' }}
+    />
   </FormControl>
 );
 
